e57b9475af3b802392a87561eb3bdcd365825e3a
Samir-ai-code <samiralonsocaro@gmail.com>
2025-07-09 15:34:09 -0400

    terremoto-ficha

--- terremoto.py
+++ terremoto.py
@@ -24,10 +24,11 @@ def leer_csv_desde_url(url, sep=',', encoding='utf-8'):
 url = "https://drive.google.com/uc?export=download&id=1UyX7sRATSNbadNxUZ7AAmbeOUm7sVpGF"
 df = leer_csv_desde_url(url)
 # st.write(df)
-# st.metric(df)
+
 mag_agno = df.nlargest(1, ['Magnitude'])['Year'].iloc[0]
 st.write(mag_agno)
 mag_valor = df.nlargest(1, ['Magnitude'])['Magnitude'].iloc[0]
 st.write(mag_valor)
 mag_ubicacion = df.nlargest(1, ['Magnitude'])['Location_Name'].iloc[0]
-
+st.metric('cabeza', 'hola', '-rata')
+st.metric(str(mag_agno),mag_valor,f"-{mag_ubicacion}")
